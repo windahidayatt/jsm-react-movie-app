b575f226ce522220ec5b9efa5af035ab0ca35a57
unknown <windahidayat27@gmail.com>
2026-07-12 14:10:29 +0700

    remove base from vite config

--- src/components/MovieCard.jsx
+++ src/components/MovieCard.jsx
@@ -13,7 +13,7 @@ const MovieCard = ({movie:
                 <h3>{title}</h3>
                 <div className="content">
                     <div className="rating">
-                        <img src='src/assets/star.svg' alt="Star Icon" />
+                        <img src='../assets/star.svg' alt="Star Icon" />
                         <p>{vote_average ? vote_average.toFixed(1) : 'N/A'}</p>
                     </div>
 
